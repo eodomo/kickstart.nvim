cba3cb8e09820631c02efcb38b45d603e230c1a8
Eric Olerud <eric@olerud.com>
2025-11-27 13:33:13 -0500

    update mason domain

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,8 +1,8 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason.nvim',
+    'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'saghen/blink.cmp', -- Auto-complete engine
     { 'folke/lazydev.nvim', opts = {} },
